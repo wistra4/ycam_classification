ad13df28adf60bd48a82fce04ce85745dfda929c
wistra4 <1946keisuke.o@keio.jp>
2021-03-14 08:31:39 +0900

    add: k_fold

--- mkds_latter.py
+++ mkds_latter.py
@@ -6,11 +6,12 @@ from pydub import AudioSegment
 import math
 import librosa
 import soundfile as sf
+import random
 
 ds_file = "/.DS_Store" # ds_storeディレクトリ名
 p_nums = []
 
-b_path = "./targets" # baseパス
+b_path = "./dataset" # baseパス
 # print(b_path)
 if os.path.exists(b_path + ds_file): # DS_Storeファイルを消去
     os.remove(b_path + ds_file)
@@ -83,17 +84,20 @@ for wav_dir in wav_dirs:
     # print("サンプリングレート：")
     # print('チャンネル数：', channel)
 
+    # sampling_rataを44100に変更
     if rate != 44100:
         y, sr = librosa.core.load(wav_path, sr=44100, mono=True)
         sf.write(wav_path, y, sr, subtype="PCM_16")
         wav_file = AudioSegment.from_file(wav_path, "wav")
         channel = wav_file.channels
 
+    # monoをstereoに変更
     if channel != 2:
         wav_file = wav_file.set_channels(2)
         wav_file.export(wav_path, format="wav")
         wav_file = AudioSegment.from_file(wav_path, "wav")
 
+    # take_alphaを付与
     num2alpha = {1:"A", 2:"B", 3:"C", 4:"D", 5:"E", 6:"F", 7:"G", 8:"H", 9:"I", 10:"J", 11:"K", 12:"L", 13:"M", 14:"N", 15:"O", 16:"P", 17:"Q", 18:"R", 19:"S", 20:"T", 21:"U", 22:"V", 23:"W", 24:"X", 25:"Y", 26:"Z"}
     for split in range(1, splits+1):
         cut_start = (split-1)*5000
@@ -117,3 +121,83 @@ for wav_dir in wav_dirs:
         wav_cut.export(take_path, format="wav")
 
     os.remove(wav_path)
+
+# mkds_4to7
+
+left = 1
+pre_idx = 1
+c_datas = []
+
+datas = os.listdir(b_path)
+datas.sort()
+# print(datas)
+for data in datas:
+    c_idx = data.split("_")[2].split(".")[0]
+    if c_idx != pre_idx:
+        print("class"+str(int(c_idx)-1))
+        print(len(c_datas))
+        k_num = 1
+        k_split = math.floor(len(c_datas)/5)
+        print(k_split)
+        k_spcopy = k_split
+        random.shuffle(c_datas)
+        # print(c_datas)
+        for c_data in c_datas:
+            # print(c_data)
+            before_path = b_path + "/" + c_data
+            if k_spcopy != 0:
+                k_spcopy -= 1
+            else:
+                k_spcopy = k_split-1
+                k_num += 1
+                
+            if k_num <= 5:
+                after_path = b_path + "/" + str(k_num) + "_" + c_data
+            elif k_num == 6:
+                print("k_num:",str(left))
+                after_path = b_path + "/" + str(left) + "_" + c_data
+                if left < 5:
+                    left += 1
+                else:
+                    left = 1
+            else:
+                print("k_num_error")
+            # print(before_path)
+            # print(after_path)
+            os.rename(before_path, after_path)
+        c_datas = []
+        c_datas.append(data)
+    else:
+        c_datas.append(data)
+    pre_idx = c_idx
+print("class"+c_idx)
+print(len(c_datas))
+k_num = 1
+k_split = math.floor(len(c_datas)/5)
+print(k_split)
+k_spcopy = k_split
+random.shuffle(c_datas)
+# print(c_datas)
+for c_data in c_datas:
+    # print(c_data)
+    before_path = b_path + "/" + c_data
+    if k_spcopy != 0:
+        k_spcopy -= 1
+    else:
+        k_spcopy = k_split-1
+        k_num += 1
+
+    if k_num <= 5:
+        after_path = b_path + "/" + str(k_num) + "_" + c_data
+    elif k_num == 6:
+        print("k_num:",str(left))
+        after_path = b_path + "/" + str(left) + "_" + c_data
+        if left < 5:
+            left += 1
+        else:
+            left = 1
+    else:
+        print("k_num_error")
+    # print(before_path)
+    # print(after_path)
+    os.rename(before_path, after_path)
